4d2c24fe10e11c450298933876caa69f4500acc6
miklzn <mikelzonsarabia@gmail.com>
2022-11-15 18:10:45 -0300

    Estructura Back

---
+++ api/config/db.js
@@ -0,0 +1,8 @@
+const sequelize = require("sequelize");
+const db = new Sequelize("houseOfDev", null, null, {
+  host: "localhost",
+  dialect: "postgres",
+  logging: false,
+});
+
+module.exports = db;
