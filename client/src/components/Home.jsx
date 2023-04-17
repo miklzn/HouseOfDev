22d63455fb2ec50f691d8eb36a2c1718556e9421
miklzn <mikelzonsarabia@gmail.com>
2023-04-17 18:14:22 -0300

    Home cards

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -17,6 +17,8 @@
 
 // import "../styles/home.css";
 import HOD_Home from "../utils/HOD-Home.svg";
+import Environment from "../utils/Environments.svg";
+import Room from "../utils/Room.svg";
 
 const Home = () => {
   //   const [properties, setProperties] = useState([]);
@@ -114,20 +116,84 @@ const Home = () => {
   //   };
 
   return (
-    <section className="h-full w-full ">
-      <div
-        className="h-screen w-screen bg-cover bg-center bg-fixed sm:h-screen "
+    <>
+      <section
+        className="h-screen w-full bg-cover bg-center bg-fixed sm:h-screen md:h-[110vh] "
         style={{
           backgroundImage: `url(https://wallpapercave.com/wp/wp1853406.jpg)`,
         }}
       >
-        <div className="h-screen flex items-center border-4 mx-[2.5vw] md:h-screen ">
-          <div className="flex w-full justify-center ">
-            <img src={HOD_Home} alt="" />
+        <div className="h-screen w-screen backdrop-opacity-10 backdrop-invert bg-transparent bg-gradient-to-b from-transparent to-black sm:h-screen md:h-[110vh]">
+          <div className="h-screen flex items-center mx-[2.5vw] pt-[33vw] sm:h-screen sm:pt-[22vw] md:h-[110vh] md:pt-[15vh] ">
+            <div className="flex flex-wrap w-full justify-center ">
+              <img src={HOD_Home} alt="" className="px-4 lg:w-1/3" />
+              <p className="text-white text-center font-dmSans px-3 py-6 md:text-lg md:px-[15vw] lg:px-[22vw]">
+                Discover the home of your dreams with us! Buy, sell or rent with
+                our wide selection of properties. Trust us to find your perfect
+                place!
+              </p>
+              <div className="w-full flex flex-col justify-center px-3 py-6 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+                <button
+                  className="block bg-primary w-full py-[1.0315rem] rounded-full text-base font-dmSans text-white hover:bg-primaryHover sm:w-auto sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.25rem] md:px-8"
+                  href="/"
+                >
+                  Explore properties
+                </button>
+                <button
+                  className="block bg-white w-full py-[1.0315rem] rounded-full text-base font-dmSans text-gray-900 hover:bg-primary hover:text-white sm:w-auto sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.25rem] md:px-8"
+                  href="/"
+                >
+                  About us
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="h-screen">
+        <div className="pt-24 mx-[5vw] mb-[4vh] space-y-8 md:flex md:flex-wrap md:items-center md:space-y-0 md:justify-between lg:mx-[10.5vw]">
+          <p className="text-2xl font-dmSans font-semibold md:text-3xl lg:text-4xl">
+            Explore our properties
+          </p>
+          <button
+            className="block shadow-button bg-white border border-gray-200 w-full py-[1rem] px-6 rounded-full text-base font-dmSans text-gray-900 hover:bg-primary hover:text-white min-[480px]:w-auto sm:w-auto sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.375rem] md:px-9"
+            href="/"
+          >
+            Browse all
+          </button>
+        </div>
+        <div className="h-full mx-[5vw]">
+          <div className="w-full h-auto border rounded-3xl">
+            <img
+              className="w-full h-[10.7rem] rounded-t-3xl"
+              src="https://assets.website-files.com/61f981dc0f719d7071d7826c/620146dffd7b262cc0983291_duplex-rustic-cabin-main-image-rental-webflow-ecommerce-template.jpg"
+              alt=""
+            />
+            <div className="px-6 pt-8 pb-7 font-dmSans">
+              <h3 className="text-xl font-semibold">Title Property</h3>
+              <div className="text-base text-gray-600 my-2">
+                Lorem ipsum dolor sit amet consectetur.
+              </div>
+              <div className="flex flex-wrap my-5">
+                <div className="w-auto flex items-center border rounded-full py-[0.625rem] px-5 text-sm">
+                  <img className="mr-2" src={Environment} alt="" /> Environments
+                </div>
+                <div className="w-auto flex items-center border border-gray-200 rounded-full py-[0.625rem] px-5 text-sm">
+                  <img className="mr-2" src={Room} alt="" /> Rooms
+                </div>
+              </div>
+              <hr className="border-t border-gray-300 my-2" />
+              <div>
+                <div className="flex items-center">
+                  <div className="text-xl font-semibold">$299</div>
+                  <div className="text-sm text-gray-400 ml-1">/month</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,10 +5,15 @@ module.exports = {
     extend: {
       colors: {
         primary: "#FF3F3F",
+        primaryHover: "#DC2B2B",
       },
       boxShadow: {
         dropdown: "0px 2px 5px rgba(0, 0, 0, 0.12)",
         nav: "0px 2px 5px rgba(0, 0, 0, 0.08)",
+        button: "0 4px 10px 0 rgba(20, 20, 43, 0.04)",
+      },
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
       },
     },
   },
