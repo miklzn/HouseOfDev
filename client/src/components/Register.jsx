271d32f7f11e45561856e2aac7ee35a7d5be4e50
miklzn <mikelzonsarabia@gmail.com>
2023-04-24 11:09:19 -0300

    Register view finished

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -74,16 +74,16 @@ const Register = () => {
     <>
       <section>
         <div>
-          <div className="relative h-[73vh] md:h-[82vh] lg:h-[76vh]">
+          <div className="relative h-[73vh] min-[480px]:h-[80vh] md:h-[82vh] lg:h-[110vh]">
             <img
               className="absolute rounded-b-3xl h-full w-full object-cover"
               src="https://latinexclusive.com/sites/default/files/api_file_downloads/5c7a20ff202ccf055be696ff_21.jpg"
               alt=""
             />
-            <div className="absolute flex w-full h-full backdrop-opacity-10 backdrop-invert bg-transparent bg-gradient-to-b from-transparent to-black/90 h-full rounded-b-3xl items-center text-center">
-              <div className="mx-[5vw] w-full text-white font-dmSans my-10 xl:px-[5vw]">
+            <div className="absolute flex w-full h-full backdrop-opacity-10 backdrop-invert bg-transparent bg-gradient-to-b from-transparent to-black/90 h-full rounded-b-3xl items-center text-center lg:items-start">
+              <div className="mx-[5vw] w-full text-white font-dmSans my-10 lg:mt-[40vh] xl:px-[5vw]">
                 <h3 className="text-[3rem] font-semibold mb-4 leading-10 min-[480px]:text-[2.375rem] md:text-5xl lg:text-6xl">
-                  Sign Up
+                  Sign up
                 </h3>
                 <p className="text-gray-100 sm:px-10 md:text-lg lg:px-40 xl:px-[20vw]">
                   Non consectetur a erat nam at lectus urna duis convallis
@@ -92,55 +92,53 @@ const Register = () => {
               </div>
             </div>
           </div>
-          <div className="mx-[5vw]">
-            <div className="sticky mb-10 lg:sticky lg:top-5 ">
+          <div className="mx-[5vw] lg:mx-[17vw] xl:mx-[28vw]">
+            <div className="sticky mb-20 lg:sticky lg:top-5 lg:mb-32">
               <div
                 className="w-full h-auto py-10 px-6 border shadow-card rounded-3xl bg-white
-              -mt-36 min-[480px]:py-12 min-[480px]:px-10 sm:py-[3.125rem] sm:px-[2rem] lg:w-auto lg:-mt-[21.5625rem]"
+              -mt-36 min-[480px]:py-12 min-[480px]:px-10 sm:py-[3.125rem] sm:px-[2rem] lg:w-auto lg:-mt-[17rem] lg:px-16 lg:py-16 xl:-mt-[21rem]"
               >
-                <h2 className="text-[1.375rem] font-semibold mb-2">
-                  Create account
-                </h2>
-                <p className="text-gray-600 mb-5 md:text-lg">
-                  Lorem ipsum dolor sit amet consectetur adipiscing elit etiam
-                  cras tellus sit.
-                </p>
-                <hr className="border-t mt-8 mb-10 " />
-                <div className="min-[480px]:flex min-[480px]:space-x-2">
+                <div className="min-[480px]:flex min-[480px]:space-x-2 md:space-x-4 md:text-lg">
                   <div className="min-[480px]:w-full">
-                    <h3 className="font-semibold mb-3">Name</h3>
-                    <div className="h-14 border rounded-full mb-4 shadow-button"></div>
+                    <div className="min-[480px]:w-full">
+                      <h3 className="font-semibold mb-3">Name</h3>
+                      <div className="h-14 border rounded-full mb-4 shadow-button"></div>
+                    </div>
+                    <div className="min-[480px]:w-full">
+                      <h3 className="font-semibold mb-3">Email</h3>
+                      <div className="h-14 border rounded-full mb-4 shadow-button"></div>
+                    </div>
                   </div>
                   <div className="min-[480px]:w-full">
-                    <h3 className="font-semibold mb-3">Email</h3>
-                    <div className="h-14 border rounded-full mb-4 shadow-button"></div>
-                  </div>
-                  <div className="min-[480px]:w-full">
-                    <h3 className="font-semibold mb-3">Phone</h3>
-                    <div className="h-14 border rounded-full mb-4 shadow-button"></div>
-                  </div>
-                  <div className="min-[480px]:w-full">
-                    <h3 className="font-semibold mb-3">Password</h3>
-                    <div className="h-14 border rounded-full mb-4 shadow-button"></div>
+                    <div className="min-[480px]:w-full">
+                      <h3 className="font-semibold mb-3">Phone</h3>
+                      <div className="h-14 border rounded-full mb-4 shadow-button"></div>
+                    </div>
+                    <div className="min-[480px]:w-full">
+                      <h3 className="font-semibold mb-3">Password</h3>
+                      <div className="h-14 border rounded-full mb-4 shadow-button"></div>
+                    </div>
                   </div>
                 </div>
-                <button
-                  className="block shadow-button bg-white border border-gray-200 w-full py-[1rem] px-6 rounded-full text-base font-dmSans text-gray-900 hover:bg-primary hover:text-white min-[480px]:w-full sm:w-full sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.375rem] md:px-9"
-                  href="/"
-                >
-                  Create
-                </button>
-                <div className="flex w-full items-center justify-center my-5">
-                  <hr className="border border-gray-300 w-1/2" />
-                  <div className="mx-5">OR</div>
-                  <hr className="border border-gray-300 w-1/2" />
+                <div className="sm:flex sm:space-x-2 sm:mt-4 md:space-x-4 lg:mt-6">
+                  <button
+                    className="block shadow-button bg-primary w-full py-[1rem] px-6 rounded-full text-base font-dmSans text-white hover:bg-primary hover:text-white min-[480px]:w-full sm:w-auto sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.375rem] md:px-9"
+                    href="/"
+                  >
+                    Submit
+                  </button>
+                  {/* <div className="flex w-full items-center justify-center my-5 sm:hidden">
+                    <hr className="border border-gray-300 w-1/2" />
+                    <div className="mx-5">OR</div>
+                    <hr className="border border-gray-300 w-1/2" />
+                  </div>
+                  <button
+                    className="block shadow-button bg-white w-full py-[1rem] px-6 rounded-full text-base font-dmSans text-gray-900 border border-gray-200 hover:bg-primary hover:text-white min-[480px]:w-full sm:w-auto sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.375rem] md:px-9"
+                    href="/"
+                  >
+                    Contact
+                  </button> */}
                 </div>
-                <button
-                  className="block shadow-button bg-black w-full py-[1rem] px-6 rounded-full text-base font-dmSans text-white hover:bg-primary hover:text-white min-[480px]:w-full sm:w-full sm:px-6 sm:py-[1.127rem] md:text-lg md:py-[1.375rem] md:px-9"
-                  href="/"
-                >
-                  Contact
-                </button>
               </div>
             </div>
           </div>
