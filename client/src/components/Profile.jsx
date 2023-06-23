f6e8cf3bdae92239ce7676ebf1839ac57af7e039
miklzn <mikelzonsarabia@gmail.com>
2023-06-23 16:56:40 -0300

    Favorites

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -9,6 +9,7 @@ import Pin from "../utils/icons/Pin.svg";
 import Email from "../utils/icons/Email.svg";
 import Smartphone from "../utils/icons/Smartphone.svg";
 import Delete from "../utils/icons/Delete.svg";
+import HeartRed from "../utils/icons/HeartRed.svg";
 import DeleteAppointmentProfile from "../commons/DeleteAppointmentProfile";
 
 const Profile = () => {
@@ -61,22 +62,24 @@ const Profile = () => {
     return setDeleteAppoint(false);
   };
 
-  //   const handleRemoveFavorite = (id) => {
-  //     axios
-  //       .post(
-  //         `http://localhost:3001/api/properties/deleteFavorites/${id}`,
-  //         {
-  //           id: id,
-  //         },
-  //         {
-  //           headers: { "Content-Type": "application/json" },
-  //           withCredentials: true,
-  //         }
-  //       )
-  //       .then((res) => dispatch(removeFavorite(res.data)))
-  //       .then(() => window.location.reload(false))
-  //       .catch((error) => console.log(error));
-  //   };
+  //-----Remove favorites
+
+  const handleRemoveFavorite = (id) => {
+    axios
+      .post(
+        `http://localhost:3001/api/properties/deleteFavorites/${id}`,
+        {
+          id: id,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((res) => dispatch(removeFavorite(res.data)))
+      .then(() => window.location.reload(false))
+      .catch((error) => console.log(error));
+  };
 
   console.log(appointments);
 
@@ -118,7 +121,7 @@ const Profile = () => {
                       setInfo(0);
                     }}
                   >
-                    Favorites (0)
+                    Favorites ({user.properties.length})
                   </button>
                   <button
                     className={
@@ -134,8 +137,8 @@ const Profile = () => {
                   </button>
                 </div>
                 {info === 0 ? (
-                  <div className="grid grid-cols gap-10 md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-x-3">
-                    {properties.map((property, i) => (
+                  <div className="grid grid-cols gap-10 md:grid-cols-2 md:gap-5 lg:gap-x-3 xl:grid-cols-3">
+                    {user.properties.map((property, i) => (
                       <div className="w-full h-auto border rounded-3xl shadow-card">
                         <img
                           className="w-full h-[53.5vw rounded-t-3xl object-cover min-[480px]:h-[48.5vw] sm:h-[55.2vw] md:h-[26.9vw] lg:h-[17.8vw]"
@@ -143,15 +146,38 @@ const Profile = () => {
                           alt=""
                         />
                         <div className="px-6 pt-8 pb-7 font-dmSans">
-                          <h3 className="text-xl font-semibold">
-                            {property.title}
-                          </h3>
-                          <div className="text-base text-gray-600 my-2">
-                            Lorem ipsum dolor sit amet consectetur.
+                          <div className="flex justify-between items-center">
+                            <div className="md:h-[115.66px] lg:h-auto lg:min-h-[100px]">
+                              <h3 className="text-xl font-semibold">
+                                {property.title}
+                              </h3>
+                              <div className="flex items-center text-base text-gray-600 my-2">
+                                <img
+                                  className="h-4 mr-2"
+                                  src="https://www.svgrepo.com/show/512655/pin-rounded-circle-620.svg"
+                                  alt=""
+                                />
+                                {property.city}, {property.country}
+                              </div>
+                            </div>
+                            <div>
+                              <button
+                                className="w-[41.6px] border rounded-full p-2 shadow-button"
+                                onClick={() =>
+                                  handleRemoveFavorite(property.id)
+                                }
+                              >
+                                <img
+                                  className="h-6 hover:scale-110"
+                                  src={HeartRed}
+                                  alt=""
+                                />
+                              </button>
+                            </div>
                           </div>
                           <div className="flex flex-wrap my-5">
                             <div className="w-auto flex items-center border rounded-full py-[0.625rem] px-4 text-sm mr-3">
-                              <img className="mr-2" src={Environment} alt="" />{" "}
+                              <img className="mr-2" src={Environment} alt="" />
                               {property.environments} Environments
                             </div>
                             <div className="w-auto flex items-center border border-gray-200 rounded-full py-[0.625rem] px-4 text-sm">
@@ -165,17 +191,22 @@ const Profile = () => {
                               <div className="text-xl font-semibold">
                                 ${property.price}
                               </div>
-                              <div className="text-sm text-gray-400 ml-1">
+                              <div
+                                className={
+                                  property.operation === "rent"
+                                    ? "text-sm text-gray-400 ml-1"
+                                    : "hidden"
+                                }
+                              >
                                 /month
                               </div>
                             </div>
                             <div className="w-full min-[480px]:w-auto">
-                              <button
-                                className="block bg-primary w-full py-[0.81rem] pl-3 pr-4 rounded-full text-white font-semibold hover:bg-primaryHover min-[480px]:w-auto min-[480px]:px-[1.63rem] md:py-4 md:px-6 md:bg-primary md:w-full md:p-0"
-                                href="/home"
-                              >
-                                See more
-                              </button>
+                              <Link to={`/properties/${property.id}`}>
+                                <button className="block bg-primary w-full py-[0.81rem] pl-3 pr-4 rounded-full text-white font-semibold hover:bg-primaryHover min-[480px]:w-auto min-[480px]:px-[1.63rem] md:py-4 md:px-6 md:bg-primary md:w-full md:p-0">
+                                  See more
+                                </button>
+                              </Link>
                             </div>
                           </div>
                         </div>
